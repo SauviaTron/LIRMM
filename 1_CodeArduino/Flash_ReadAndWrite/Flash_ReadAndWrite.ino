13c28213a073c4f449a54cf81febddbe8b482004
Andrea Sauviat <andrea.sauviat@lirmm.fr>
2023-03-01 10:39:19 +0100

    Flash R&W - Code Cleaning

--- 1_CodeArduino/Flash_ReadAndWrite/Flash_ReadAndWrite.ino
+++ 1_CodeArduino/Flash_ReadAndWrite/Flash_ReadAndWrite.ino
@@ -19,6 +19,8 @@
 #include "src/STM32L0_Custom.h"  // Management of the STM32L082CZ
 #include "src/Flash_ReadWrite.h" // Management of the 196k Flash of the STM32L082CZ
 
+#define Debug_Mode    false
+
 //uint32_t address = 0x08021980; // adresse de départ de la mémoire flash
 uint8_t data[128]; // tableau pour stocker les données lues
 uint32_t count = 128; // nombre de données à lire
@@ -34,16 +36,14 @@ uint32_t flashAddress_Updated = flashAddress ;
 void setup() {
 
   STM32L0.wakeup() ;
+
+  if( Debug_Mode == true ){ while( !Serial ){} }
   
-  // put your setup code here, to run once:
   Serial.begin(115200) ; 
 
-  delay(5000) ;
+  delay(1000) ;
 
   STM32L0.flashErase( flashAddress , 2 ) ;
-
-
-
 
 }
 
@@ -56,80 +56,34 @@ void setup() {
 
 void loop() {
   
-  Serial.println("Hello Flash_Read.ino") ;
+  if( 1 ){
 
-    if( 1 ){
-
-    Serial.print( "flashAddress : " ) ; Serial.println( flashAddress_Updated , HEX ) ;
+    Serial.print( "Time flashAddress \t" ) ; Serial.print( flashAddress_Updated , HEX ) ; Serial.print("\t") ;
     flashAddress_Updated = Flash_PushToMemory_Time( 2302101615 , flashAddress_Updated , true ) ;
-    Serial.print( "flashAddress : " ) ; Serial.println( flashAddress_Updated , HEX ) ;
-    flashAddress_Updated = Flash_PushToMemory_GPS( 43000000 , 3000000 , 15 , flashAddress_Updated , true ) ;
-    Serial.print( "flashAddress : " ) ; Serial.println( flashAddress_Updated , HEX ) ;
 
-    // flashAddress = 0x8021980 ;
+    Serial.print( "GPS flashAddress \t" ) ; Serial.print( flashAddress_Updated , HEX ) ; Serial.print("\t") ;
+    flashAddress_Updated = Flash_PushToMemory_GPS( 43000000 , 300000015 , flashAddress_Updated , true ) ;
 
+    Serial.print( "Acc flashAddress \t" ) ; Serial.print( flashAddress_Updated , HEX ) ; Serial.print("\t") ;
+    flashAddress_Updated = Flash_PushToMemory_Acc( 1 , 2 , 3 , 15 , flashAddress_Updated , true ) ;
+
+    Serial.print( "Other flashAddress \t" ) ; Serial.print( flashAddress_Updated , HEX ) ; Serial.print("\t") ;
+    flashAddress_Updated = Flash_PushToMemory_Vbat_LoRa( 375 , 0 , 1 , flashAddress_Updated , true ) ;
+
+
+    
     if( 1 ){
 
     Serial.print( "Flash ReadAddress : " ) ; Serial.println( 0x8021980 , HEX ) ;
 
     STM32L0.flashRead(flashAddress, data, count);
 
-    Serial.println("Données lues à partir de la mémoire flash : ");
-    int Nb_Data_Display = 0 ;
-    int Flash_Nb_Cell = 1 ;
-    for (int i = 0; i < count; i++) {
-      
-        if( Nb_Data_Display == 0 ){ 
-          if( Flash_Nb_Cell <= 9 ){
-            Serial.print( (String)"Cell n°0" + Flash_Nb_Cell) ; Serial.print("\t") ;
-            Flash_Nb_Cell += 1 ; 
-          }
-          else{
-            Serial.print( (String)"Cell n°" + Flash_Nb_Cell) ; Serial.print("\t") ;
-            Flash_Nb_Cell += 1 ; 
-          }
-        }
-        
-        String data_i_string = String( data[i] , BIN ) ;
-        // Serial.println( (String)"Taille string : " + data_i_string ) ; 
-        switch( data_i_string.length() ){
-          case 8 :
-            Serial.print(data[i], BIN); Serial.print(" ");
-            break ;
-          case 7 :
-            Serial.print( "0" ) ; Serial.print(data[i], BIN); Serial.print(" ");
-            break ;
-          case 6 :
-            Serial.print( "00" ) ; Serial.print(data[i], BIN); Serial.print(" ");
-            break ;
-          case 5 :
-            Serial.print( "000" ) ; Serial.print(data[i], BIN); Serial.print(" ");
-            break ;
-          case 4 :
-            Serial.print( "0000" ) ; Serial.print(data[i], BIN); Serial.print(" ");
-            break ;
-          case 3 :
-            Serial.print( "00000" ) ; Serial.print(data[i], BIN); Serial.print(" ");
-            break ;
-          case 2 :
-            Serial.print( "000000" ) ; Serial.print(data[i], BIN); Serial.print(" ");
-            break ;
-          case 1 :
-            Serial.print( "0000000" ) ; Serial.print(data[i], BIN); Serial.print(" ");
-            break ;
-        } // switch( data_i_string.length() )
+    STM32L0.Flash_Print_Data( data , count ) ;
 
-        Nb_Data_Display += 1 ;
-        if( Nb_Data_Display == 4 ){
-          Serial.println(" "); 
-          Nb_Data_Display = 0 ;
-        }
-
-    }
     Serial.println();
 
     }
-  
+
   }
 
   delay(10000) ;
